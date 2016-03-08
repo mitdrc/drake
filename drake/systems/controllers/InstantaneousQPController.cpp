aaf1fe0e6a67a7e631de40986acd404f83932136
Gregory Izatt <gizatt@mit.edu>
2016-03-08 17:32:01 -0500

    Accessors for utilities needed for DRC-side control.

--- drake/systems/controllers/InstantaneousQPController.cpp
+++ drake/systems/controllers/InstantaneousQPController.cpp
@@ -105,6 +105,7 @@ void InstantaneousQPController::loadConfigurationFromYAML(
                                 debug_file);
   rpc = parseKinematicTreeMetadata(control_config["kinematic_tree_metadata"],
                                    *robot);
+  input_joint_names = parseRobotJointNames(control_config["joint_names"], *robot);
 }
 
 void applyURDFModifications(std::unique_ptr<RigidBodyTree>& robot,
@@ -582,6 +583,12 @@ std::unordered_map<std::string, int> computeBodyOrFrameNameToIdMap(
   return id_map;
 }
 
+void InstantaneousQPController::resetControllerState(double t_new){
+  this->controller_state.t_prev = t_new;
+  this->controller_state.vref_integrator_state = VectorXd::Zero(this->controller_state.vref_integrator_state.size());
+  this->controller_state.q_integrator_state = VectorXd::Zero(this->controller_state.q_integrator_state.size());
+}
+
 const QPControllerParams& InstantaneousQPController::FindParams(
     const std::string& param_set_name) {
   // look up the param set by name
@@ -597,10 +604,8 @@ const QPControllerParams& InstantaneousQPController::FindParams(
           "up here.");
     }
   }
-  // cout << "using params set: " + it->first + ", ";
-  const QPControllerParams& params = it->second;
-  // mexPrintf("Kp_accel: %f, ", params.Kp_accel);
-  return params;
+
+  return it->second;
 }
 
 int InstantaneousQPController::setupAndSolveQP(
--- drake/util/yaml/yamlUtil.h
+++ drake/util/yaml/yamlUtil.h
@@ -26,3 +26,5 @@ std::map<std::string, QPControllerParams> loadAllParamSets(
 RobotPropertyCache parseKinematicTreeMetadata(const YAML::Node& metadata,
                                               const RigidBodyTree& robot);
 KinematicModifications parseKinematicModifications(const YAML::Node& mods);
+JointNames parseRobotJointNames(const YAML::Node& joint_names,
+                                const RigidBodyTree& tree);
